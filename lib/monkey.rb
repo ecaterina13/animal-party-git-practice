d36b52caa0142d147e0df42519efd66cbc6806da
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-02-19 14:23:21 -0700

    make monkey class

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -1,3 +1,10 @@
 class Monkey
-
+    attr_reader :name, :pen_number
+ def initialize(name, pen_number)
+    @name = name
+    @pen_number = pen_number
+ end
+ def taxon_class
+    :mammal
+ end
 end
